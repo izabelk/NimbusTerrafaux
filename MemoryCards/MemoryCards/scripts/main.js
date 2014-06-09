e3d4d2c7b239564b88c3a06a878078d90f69b066
izabelk <izi_girl@abv.bg>
2014-06-09 15:31:06 +0300

    added example menu, it code needs to be refactored

--- MemoryCards/MemoryCards/scripts/main.js
+++ MemoryCards/MemoryCards/scripts/main.js
@@ -6,10 +6,16 @@ var stage = new Kinetic.Stage({
     height: 600
 });
 
+//creates the menu
+var layerOfMenu = new Kinetic.Layer();
+layerOfMenu.setWidth(800);
+layerOfMenu.setHeight(600);
+
 // create the canvas
-var layer = new Kinetic.Layer();
-layer.setWidth(800);
-layer.setHeight(600);
+var layerOfGame = new Kinetic.Layer();
+layerOfGame.setWidth(800);
+layerOfGame.setHeight(600);
+
 var layerOffset = 10;
 
 var rows = 4;
@@ -19,8 +25,8 @@ var cardOffset = 10;
 var initialXOffset = (cardOffset + layerOffset) * 2;
 var initialYOffset = (cardOffset + layerOffset) * 2;
 // if we want cards to be centered
-// var initialXOffset = (layer.getWidth() - cols * Card.DIMENSION.width - cols * cardOffset) / 2;
-// var initialYOffset = (layer.getHeight() - rows * Card.DIMENSION.height - rows * cardOffset) / 2;
+// var initialXOffset = (layerOfGame.getWidth() - cols * Card.DIMENSION.width - cols * cardOffset) / 2;
+// var initialYOffset = (layerOfGame.getHeight() - rows * Card.DIMENSION.height - rows * cardOffset) / 2;
 
 function createCards() {
 
@@ -40,29 +46,158 @@ function createCards() {
 
 var cards = createCards();
 
-// draw field
-(function initialize() {
+(function initializeMenu() {
+
     var rect = new Kinetic.Rect({
         x: layerOffset,
         y: layerOffset,
-        width: layer.getWidth() - layerOffset * 2,
-        height: layer.getHeight() - layerOffset * 2,
+        width: layerOfMenu.getWidth() - layerOffset * 2,
+        height: layerOfMenu.getHeight() - layerOffset * 2,
+        visible: true,
+        fill: '#B5FFDE',
+        stroke: '#51A5FF',
+        strokeWidth: 4,
+        cornerRadius: 10
+    });
+
+    var title = new Kinetic.Text({
+        text: 'Memory Cards',
+        fontFamily: 'French Script MT',
+        fontSize: 60,
+        x: 240,
+        y: 50,
+        fill: '#3538FF'
+    });
+
+    var subTitle = new Kinetic.Text({
+        text: 'by Nimbus Terrafaux',
+        fontFamily: 'French Script MT',
+        fontSize: 40,
+        x: 380,
+        y: 110,
+        fill: '#3538FF'
+    });
+
+    var newGameRectVisible = new Kinetic.Rect({
+        x: 250,
+        y: 230,
+        width: 200,
+        height: 50,
+        stroke: '#3538FF',
+        fill: '',
+        cornerRadius: 10
+    });
+
+    var invisibleRectNewGame = new Kinetic.Rect({
+        x: 250,
+        y: 230,
+        width: 200,
+        height: 50,
+        opacity: 0,
+        cornerRadius: 10
+    });
+
+    var newGameText = new Kinetic.Text({
+        text: 'New Game',
+        fontFamily: 'Arial',
+        fontSize: 20,
+        x: 300,
+        y: 245,
+        fill: '#3538FF'
+    });
+
+    invisibleRectNewGame.on('mouseover', function () {
+        newGameRectVisible.fill('#CCFFFF');
+        layerOfMenu.draw();
+    });
+
+    invisibleRectNewGame.on('mouseout', function () {
+        newGameRectVisible.fill('#B5FFDE');
+        layerOfMenu.draw();
+    });
+
+    invisibleRectNewGame.on('click', function () {
+        layerOfGame.visible(true);
+        layerOfMenu.visible(false);
+        initializeField();
+        anim.start();
+    });
+
+    var highScoresRectVisible = new Kinetic.Rect({
+        x: 250,
+        y: 290,
+        width: 200,
+        height: 50,
+        stroke: '#3538FF',
+        fill: '',
+        cornerRadius: 10
+    });
+
+    var highScoresText = new Kinetic.Text({
+        text: 'High Scores',
+        fontFamily: 'Arial',
+        fontSize: 20,
+        x: 300,
+        y: 305,
+        fill: '#3538FF'
+    });
+
+    var invisibleRectScores = new Kinetic.Rect({
+        x: 250,
+        y: 290,
+        width: 200,
+        height: 50,
+        opacity: 0,
+        cornerRadius: 10
+    });
+
+    invisibleRectScores.on('mouseover', function () {
+        highScoresRectVisible.fill('#CCFFFF');
+        layerOfMenu.draw();
+    });
+
+    invisibleRectScores.on('mouseout', function () {
+        highScoresRectVisible.fill('#B5FFDE');
+        layerOfMenu.draw();
+    });
+
+    layerOfMenu.add(rect);
+    layerOfMenu.add(title);
+    layerOfMenu.add(subTitle);
+    layerOfMenu.add(newGameRectVisible);
+    layerOfMenu.add(newGameText);
+    layerOfMenu.add(invisibleRectNewGame);
+    layerOfMenu.add(highScoresRectVisible);
+    layerOfMenu.add(highScoresText);
+    layerOfMenu.add(invisibleRectScores);
+
+})();
+
+// draw field
+function initializeField() {
+    var rect = new Kinetic.Rect({
+        x: layerOffset,
+        y: layerOffset,
+        width: layerOfGame.getWidth() - layerOffset * 2,
+        height: layerOfGame.getHeight() - layerOffset * 2,
+        visible : true,
         fill: 'lightblue',
         stroke: 'black',
         strokeWidth: 4
     }); // Create table
 
-    layer.add(rect);
+    layerOfGame.add(rect);
+    //layerOfGame.draw();
 
     for (var i = 0; i < cards.length; i++) {
-        cards[i].draw(layer);
+        cards[i].draw(layerOfGame);
     }
-})();
+}
 
 var current = [];
 
 // click on card
-layer.on('click', function (ev) {
+layerOfGame.on('click', function (ev) {
 
     var fx = ev.evt.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
     var fy = ev.evt.clientY + document.body.scrollTop + document.documentElement.scrollTop;
@@ -107,11 +242,12 @@ layer.on('click', function (ev) {
 var anim = new Kinetic.Animation(function (frame) {
     for (var i = 0; i < cards.length; i++) {
 
-        cards[i].draw(layer);
+        cards[i].draw(layerOfGame);
     }
 
-}, layer);
+}, layerOfGame);
 
-anim.start();
+//anim.start();
 
-stage.add(layer);
+stage.add(layerOfMenu);
+stage.add(layerOfGame);
